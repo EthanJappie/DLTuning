cf8a62ae86787d303977ab6a4b6f0cecb201654f
Ethan <ethan@warpdevelopment.co.za>
2018-10-08 08:21:47 +0200

    first commit

---
+++ src/app/firebase.ts
@@ -0,0 +1,7 @@
+export var config = {
+    apiKey: "AIzaSyAaEJNiLqlDMzSCP1OLb7iJeUOUpzD3-6I",
+    authDomain: "dltuning-1.firebaseapp.com",
+    databaseURL: "https://dltuning-1.firebaseio.com",
+    projectId: "dltuning-1",
+    storageBucket: "dltuning-1.appspot.com",
+    messagingSenderId: "691398870661"}
